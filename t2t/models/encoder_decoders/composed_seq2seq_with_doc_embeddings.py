b419255fd6b2f3cafa4ee136c150f5272d8261ca
johngiorgi <johnmgiorgi@gmail.com>
2019-12-27 15:17:20 -0500

    :recycle: Reformat code for new max line length

--- t2t/models/encoder_decoders/composed_seq2seq_with_doc_embeddings.py
+++ t2t/models/encoder_decoders/composed_seq2seq_with_doc_embeddings.py
@@ -10,9 +10,9 @@ from allennlp.models.model import Model
 @Model.register("composed_seq2seq_with_doc_embeddings")
 class ComposedSeq2SeqWithDocEmbeddings(ComposedSeq2Seq):
     """
-    A thin wrapper around ``ComposedSeq2Seq`` which adds the document embeddings learned by the
-    model to its ``output_dict`` in the ``forward`` hook. See the ``ComposedSeq2Seq model in the
-    AllenNLP documentation for full details.
+    A thin wrapper around ``ComposedSeq2Seq`` which adds the document embeddings learned by the model to its
+    ``output_dict`` in the ``forward`` hook. See the ``ComposedSeq2Seq`` model in the AllenNLP documentation for
+    full details.
     """
 
     @overrides
@@ -28,11 +28,11 @@ class ComposedSeq2SeqWithDocEmbeddings(ComposedSeq2Seq):
         Parameters
         ----------
         source_tokens : ``Dict[str, torch.LongTensor]``
-           The output of `TextField.as_array()` applied on the source `TextField`. This will be
-           passed through a `TextFieldEmbedder` and then through an encoder.
+           The output of `TextField.as_array()` applied on the source `TextField`. This will be passed through a
+           `TextFieldEmbedder` and then through an encoder.
         target_tokens : ``Dict[str, torch.LongTensor]``, optional (default = None)
-           Output of `Textfield.as_array()` applied on target `TextField`. We assume that the
-           target tokens are also represented as a `TextField`.
+           Output of `Textfield.as_array()` applied on target `TextField`. We assume that the target tokens are
+           also represented as a `TextField`.
 
         Returns
         -------
@@ -45,7 +45,6 @@ class ComposedSeq2SeqWithDocEmbeddings(ComposedSeq2Seq):
         # During eval, copy the encoders output to CPU memory. This becomes our document embedding.
         # HACK (John): Document embeddings are expanded along the second dimension, drop it
         if not self.training:
-            output_dict['doc_embeddings'] = \
-                state['encoder_outputs'][:, 0, :].cpu().squeeze(1)
+            output_dict['doc_embeddings'] = state['encoder_outputs'][:, 0, :].cpu().squeeze(1)
 
         return output_dict

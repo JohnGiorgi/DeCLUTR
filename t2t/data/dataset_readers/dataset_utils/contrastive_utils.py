15219a838d1e47ed005facc52e7acb237f9411ab
johngiorgi <johnmgiorgi@gmail.com>
2020-03-04 19:19:50 -0500

    :zap: Dramatic speedup sampling spans

---
+++ t2t/data/dataset_readers/dataset_utils/contrastive_utils.py
@@ -0,0 +1,32 @@
+from random import randint
+from typing import Iterable
+
+
+def sample_spans(text: str, num_spans: int, min_span_width: int) -> Iterable[str]:
+    """Returns a generator that yields random spans from `text`.
+
+    # Parameters
+
+    text : `str`, required
+        The string to extract spans from.
+    num_spans : ``int``, required
+        The total number of spans to return.
+    min_span_width : `int`, required
+        The minimum length of spans, after whitespace tokenization, which should be sampled.
+    """
+
+    # No need for fancier tokenization here. Whitespace tokenization will suffice to generate spans.
+    tokens = text.split()
+
+    if min_span_width > len(tokens):
+        raise ValueError(
+            (
+                f"min_span_width ({min_span_width}) must be less than or equal to len(text.split())"
+                f" ({len(tokens)})."
+            )
+        )
+
+    for _ in range(num_spans):
+        start = randint(0, len(tokens) - min_span_width)
+        end = randint(start + min_span_width, len(tokens)) if start + min_span_width < len(tokens) else len(tokens)
+        yield " ".join(tokens[start:end])

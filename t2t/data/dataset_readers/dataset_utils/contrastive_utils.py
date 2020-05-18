e8ae5019549210460c8e7aa3d605e9db1cb67ea8
johngiorgi <johnmgiorgi@gmail.com>
2020-05-18 16:36:55 -0400

    :recycle: More robust adjacent selection

--- t2t/data/dataset_readers/dataset_utils/contrastive_utils.py
+++ t2t/data/dataset_readers/dataset_utils/contrastive_utils.py
@@ -1,13 +1,11 @@
-from enum import Enum
 from random import choice, randint
 from typing import Callable, List, Optional, Tuple
 
 import numpy as np
 
+from allennlp.common.logging import AllenNlpLogger
 
-class Strategy(str, Enum):
-    subsuming = "subsuming"
-    adjacent = "adjacent"
+logger = AllenNlpLogger(__name__)
 
 
 def sample_anchor_positives(
@@ -15,7 +13,7 @@ def sample_anchor_positives(
     max_span_len: int,
     min_span_len: int,
     num_spans: Optional[int] = 1,
-    strategy: Optional[Strategy] = None,
+    sampling_strategy: Optional[str] = None,
     tokenizer: Optional[Callable[[str], List[str]]] = None,
 ) -> Tuple[str, List[str]]:
     """Returns a tuple of anchor (`str`) and `num_spans` positive (`List[str]`) spans sampled from
@@ -31,22 +29,25 @@ def sample_anchor_positives(
         The minimum length of spans, after tokenization, to sample.
     num_spans : `int`, optional (default = 1)
         The number of spans to sample from `text` to serve as positive examples.
-    strategy : `Strategy`, optional (default = None)
-        One of "subsuming" or "adjacent". If "subsuming", positive spans are always subsumed by
-        the anchor. If "adjacent", positive spans are always adjacent to the anchor. If not
-        provided, positives may be subsumed, adjacent to, or overlapping with the anchor.
+    sampling_strategy : `str`, optional (default = None)
+        One of "subsuming" or "adjacent". If "subsuming," positive spans are always subsumed by the
+        anchor. If "adjacent", positive spans are always adjacent to the anchor. If not provided,
+        positives may be subsumed, adjacent to, or overlapping with the anchor. Has no effect if
+        `num_spans` is not provided.
     tokenizer : `Callable`, optional
         Optional tokenizer to use before sampling spans. If `None`, `text.split()` is used.
     """
     # Whitespace tokenization is much more straightforward (don't need to worry about chopping up
-    # subword tokens) but a user can also provide their own tokenization scheme if they want.
+    # subword tokens), but a user can also provide their own tokenization scheme if they want.
     tokens = tokenizer(text) if tokenizer is not None else text.split()
     num_tokens = len(tokens)
     tok_method = "tokenizer(text)" if tokenizer else "text.split()"
 
-    if num_tokens < 1:
+    # This is mostly arbitrary, but it prevents the Hypothesis tests from breaking. And it makes
+    # little sense to sample from extremely short documents.
+    if num_tokens < 10:
         raise ValueError(
-            (f"len({tok_method}) should be at least 1 (ideally much longer), got {num_tokens}.")
+            (f"len({tok_method}) should be at least 10 (ideally much longer), got {num_tokens}.")
         )
     if min_span_len > max_span_len:
         raise ValueError(
@@ -69,40 +70,50 @@ def sample_anchor_positives(
     anchor = " ".join(tokens[anchor_start:anchor_end])
 
     # Sample positives from around the anchor. The intuition being that text that appears close
-    # together is the same document is likely to be semantically similar. By default, these may be
-    # adjacent or overlap with each other and the anchor, but this can be selected manually with
-    # the `strategy` parameter. Their length is sampled from a beta distribution skewed towards
-    # shorter spans.
+    # together is the same document is likely to be semantically similar.
     positives = []
     for _ in range(num_spans):
+        # Their length is sampled from a beta distribution skewed towards shorter spans. The idea
+        # is to promote diversity and minimize the amount of overlapping text.
         positive_length = int(np.random.beta(2, 4) * (max_span_len - min_span_len) + min_span_len)
-        if strategy and strategy == strategy.subsuming:
+        # A user can specify a subsuming or adjacent only sampling strategy.
+        if sampling_strategy == "subsuming":
             positive_start = randint(anchor_start, anchor_end - positive_length)
-        elif strategy and strategy == strategy.adjacent:
-            # There are two types of adjacent positives, those that boarder the beginning of the
-            # anchor and those that border the end. These may not be valid in cases where the
-            # positive length is longer than the available space in the document.
+        elif sampling_strategy == "adjacent":
+            # We have to restrict positives to a length that will allow them to be adjacent to
+            # the anchor without running off the edge of the document. If documents are sufficiently
+            # long, this won't be a problem and the max_positive_length will equal max_span_len.
+            max_positive_len = min(max_span_len, max(anchor_start, num_tokens - anchor_end))
+            if positive_length > max_positive_len:
+                logger.warning_once(
+                    (
+                        f"There is no room to sample an adjacent positive span of"
+                        f" max_span_len {positive_length}. Temporarily reducing the"
+                        f" maximum span length of positives to {max_positive_len}."
+                        " This message will not be displayed again."
+                    )
+                )
+            positive_length = int(
+                np.random.beta(2, 4) * (max_positive_len - min_span_len) + min_span_len
+            )
+            # There are two types of adjacent positives, those that border the beginning of the
+            # anchor and those that border the end. The checks above guarantee at least one of these
+            # is valid.
             valid_starts = []
             if anchor_start - positive_length > 0:
                 valid_starts.append(anchor_start - positive_length)
             if anchor_end + positive_length <= num_tokens:
                 valid_starts.append(anchor_end)
-            if not valid_starts:
-                raise ValueError(
-                    (
-                        f"Cannot select adjacent only spans for a span length of {positive_length}"
-                        f" and a number of tokens len({tok_method}) ({num_tokens})."
-                    )
-                )
             positive_start = choice(valid_starts)
         else:
+            # By default, spans may be adjacent or overlap with each other and the anchor.
             # Be careful not to run off the edges of the document, as this error will pass silently.
             positive_start = randint(
                 max(0, anchor_start - positive_length),
                 min(anchor_end, num_tokens - positive_length),
             )
 
-    positive_end = positive_start + positive_length
-    positives.append(" ".join(tokens[positive_start:positive_end]))
+        positive_end = positive_start + positive_length
+        positives.append(" ".join(tokens[positive_start:positive_end]))
 
     return anchor, positives

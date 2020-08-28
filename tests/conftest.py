acf00875ad9abdace2e5e25ddc59538efb7fb3fd
johngiorgi <johnmgiorgi@gmail.com>
2020-08-27 20:28:41 -0400

    :white_check_mark: Scope fixtures at module level to prevent warnings

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,9 @@ from allennlp.predictors import Predictor
 from declutr.encoder import PRETRAINED_MODELS, Encoder
 from declutr.predictor import DeCLUTRPredictor
 
+# Note: Most of these are scoped as "module" to prevent a warning from hypothesis
+# about fixtures being reset between function calls.
+
 
 @pytest.fixture(params=["declutr-small", "declutr-base"], scope="module")
 def archive(request) -> Archive:
@@ -30,13 +33,13 @@ def encoder(request) -> Encoder:
     return Encoder(request.param)
 
 
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def inputs_filepath() -> str:
     # Some random examples taken from https://nlp.stanford.edu/projects/snli/
     return "tests/fixtures/data/encoder_inputs.txt"
 
 
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def inputs(inputs_filepath) -> List[str]:
     # Some random examples taken from https://nlp.stanford.edu/projects/snli/
     return Path(inputs_filepath).read_text().split("\n")

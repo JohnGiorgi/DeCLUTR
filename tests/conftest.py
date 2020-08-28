80181cecb8774fe3d11f61aba67613d44e3841cf
johngiorgi <johnmgiorgi@gmail.com>
2020-08-27 20:20:09 -0400

    :white_check_mark: Update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import List
 
 import pytest
@@ -29,16 +30,13 @@ def encoder(request) -> Encoder:
     return Encoder(request.param)
 
 
-@pytest.fixture(scope="module")
-def inputs() -> List[str]:
+@pytest.fixture()
+def inputs_filepath() -> str:
     # Some random examples taken from https://nlp.stanford.edu/projects/snli/
-    return [
-        "A man inspects the uniform of a figure in some East Asian country.",
-        "The man is sleeping",
-        "An older and younger man smiling.",
-        "Two men are smiling and laughing at the cats playing on the floor.",
-        "A black race car starts up in front of a crowd of people.",
-        "A man is driving down a lonely road.",
-        "A smiling costumed woman is holding an umbrella.",
-        "A happy woman in a fairy costume holds an umbrella.",
-    ]
+    return "tests/fixtures/data/encoder_inputs.txt"
+
+
+@pytest.fixture()
+def inputs(inputs_filepath) -> List[str]:
+    # Some random examples taken from https://nlp.stanford.edu/projects/snli/
+    return Path(inputs_filepath).read_text().split("\n")

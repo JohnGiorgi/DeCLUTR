839f00dc3d7efe2337c2d9713c861fd98af497e2
johngiorgi <johnmgiorgi@gmail.com>
2020-10-28 18:29:25 -0400

    :white_check_mark: Fix bad tests

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -2,14 +2,16 @@ from pathlib import Path
 from typing import List
 
 import pytest
+import torch
+from declutr import Encoder
 from hypothesis import given, settings
 from hypothesis.strategies import booleans
-from scipy.spatial.distance import cosine
-
-from declutr import Encoder
+from torch.nn import CosineSimilarity
 
 
 class TestEncoder:
+    cosine = CosineSimilarity(dim=-1)
+
     # The base model will take longer than the small model, which triggers a test timing error.
     # Turn off deadlines to avoid this.
     @settings(deadline=None)
@@ -23,8 +25,10 @@ class TestEncoder:
         # Run three distinct tests, which should cover all use cases of Encoder:
         #  1. A List[str] input where batch_size is not None.
         embeddings = encoder(inputs, batch_size=len(inputs))
-        assert cosine(embeddings[0], embeddings[1]) > cosine(embeddings[1], embeddings[2])
-        assert cosine(embeddings[0], embeddings[2]) > cosine(embeddings[1], embeddings[2])
+        embeddings = torch.from_numpy(embeddings)
+        # These are hard-coded examples that should have the highest cosine similarity.
+        assert torch.topk(self.cosine(embeddings[2], embeddings), k=2)[-1][-1].item() == 3
+        assert torch.topk(self.cosine(embeddings[6], embeddings), k=2)[-1][-1].item() == 7
 
         #  2. A str input where batch_size is None. Check that the expected UserWarning is raised.
         embeddings = []
@@ -34,10 +38,12 @@ class TestEncoder:
                     embeddings.append(encoder(text, batch_size=None))
             else:
                 embeddings.append(encoder(text, batch_size=None))
-        assert cosine(embeddings[0], embeddings[1]) > cosine(embeddings[1], embeddings[2])
-        assert cosine(embeddings[0], embeddings[2]) > cosine(embeddings[1], embeddings[2])
+        embeddings = torch.as_tensor(embeddings).squeeze(1)
+        assert torch.topk(self.cosine(embeddings[2], embeddings), k=2)[-1][-1].item() == 3
+        assert torch.topk(self.cosine(embeddings[6], embeddings), k=2)[-1][-1].item() == 7
 
         #  3. A filepath input that points to file with one example per line.
         embeddings = encoder(inputs_filepath, batch_size=len(inputs))
-        assert cosine(embeddings[0], embeddings[1]) > cosine(embeddings[1], embeddings[2])
-        assert cosine(embeddings[0], embeddings[2]) > cosine(embeddings[1], embeddings[2])
+        embeddings = torch.from_numpy(embeddings)
+        assert torch.topk(self.cosine(embeddings[2], embeddings), k=2)[-1][-1].item() == 3
+        assert torch.topk(self.cosine(embeddings[6], embeddings), k=2)[-1][-1].item() == 7

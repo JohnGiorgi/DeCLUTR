c41f37cf8fb0d8f92cbaf15a14a0f1d305cfe303
johngiorgi <johnmgiorgi@gmail.com>
2021-05-12 15:30:08 -0400

    :white_check_mark: Update tests

--- tests/common/test_model_utils.py
+++ tests/common/test_model_utils.py
@@ -5,7 +5,7 @@ from allennlp.data import TextFieldTensors
 from hypothesis import given, settings
 from hypothesis.strategies import integers
 
-from declutr.common.model_utils import unpack_batch
+from declutr.common import model_utils
 
 
 class TestModelUtils:
@@ -44,16 +44,33 @@ class TestModelUtils:
 
         # Only TextFieldTensors with tensors of three dimensions should be reshaped...
         # Tensors are updated in-place, so deepcopy before passing to unpack_batch
-        actual_three_input_dim = unpack_batch(deepcopy(three_dim_input))
+        actual_three_input_dim = model_utils.unpack_batch(deepcopy(three_dim_input))
         for name, tensor in actual_three_input_dim["tokens"].items():
             assert torch.equal(
                 tensor,
                 three_dim_input["tokens"][name].reshape(batch_size * num_anchors, max_length),
             )
         # ...unpack_batch is a no-op for TextFieldTensors with tensors less than or greater than 3D.
-        actual_two_dim_input = unpack_batch(deepcopy(two_dim_input))
+        actual_two_dim_input = model_utils.unpack_batch(deepcopy(two_dim_input))
         for name, tensor in actual_two_dim_input["tokens"].items():
             assert torch.equal(tensor, two_dim_input["tokens"][name])
-        actual_four_dim_input = unpack_batch(deepcopy(four_dim_input))
+        actual_four_dim_input = model_utils.unpack_batch(deepcopy(four_dim_input))
         for name, tensor in actual_four_dim_input["tokens"].items():
             assert torch.equal(tensor, four_dim_input["tokens"][name])
+
+    def test_all_gather_anchor_positive_pairs_no_op(self) -> None:
+        """Check that `all_gather_anchor_positive_pairs` is a no-op when not in distributed mode.
+        """
+        num_anchors = 2
+        num_positives = 2
+        batch_size = 16
+        embedding_dim = 256
+
+        expected_anchors = torch.randn(num_anchors, batch_size, embedding_dim)
+        expected_positives = torch.randn(num_positives, batch_size, embedding_dim)
+        actual_anchors, actual_positives = model_utils.all_gather_anchor_positive_pairs(
+            expected_anchors, expected_positives
+        )
+
+        assert torch.equal(actual_anchors, expected_anchors)
+        assert torch.equal(actual_positives, expected_positives)

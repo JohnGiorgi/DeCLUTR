1ad868d758db82ab78ff644a91c79936aadb63ee
johngiorgi <johnmgiorgi@gmail.com>
2021-05-12 15:10:14 -0400

    :zap: Batch inputs to HF tokenizer

--- scripts/preprocess_openwebtext.py
+++ scripts/preprocess_openwebtext.py
@@ -2,10 +2,11 @@
 import shutil
 import tarfile
 from pathlib import Path
-from typing import List, Optional
+from typing import Optional
 
 import typer
-from declutr.common.data_utils import sanitize
+from declutr.common.util import sanitize_text
+from more_itertools import chunked
 
 # Emoji's used in typer.secho calls
 # See: https://github.com/carpedm20/emoji/blob/master/emoji/unicode_codes.py"
@@ -14,34 +15,24 @@ SAVING = "\U0001F4BE"
 MINING = "\U000026CF"
 
 
-def _write_output_to_disk(text: List[str], output_filepath: Path) -> None:
-    """Writes a list of documents, `text`, to the file `output_filepath`, one document per line."""
-    # Create the directory path if it doesn't exist
-    output_filepath = Path(output_filepath)
-    output_filepath.parents[0].mkdir(parents=True, exist_ok=True)
-
-    with open(output_filepath, "w") as f:
-        # In the future, it might make sense to both batch and shard:
-        # 1) Batch, meaning write batches of documents to a file as opposed to 1 at a time
-        # 2) Shard, meaning break a file up into shard_size // len(text) files, and return a
-        #    directory instead. Loading a dataset like this is supported in AllenNLP (see:
-        #    https://docs.allennlp.org/master/api/data/dataset_readers/sharded_dataset_reader/)
-        with typer.progressbar(text, label="Writing to disk") as progress:
-            for doc in progress:
-                f.write(doc.strip() + "\n")
-    typer.secho(
-        f"{SAVING} {len(text)} preprocessed documents saved to: {output_filepath}",
-        bold=True,
-    )
-
-
 def main(
-    openwebtext_path: Path,
-    output_filepath: Path,
-    min_length: Optional[int] = None,
-    lowercase: bool = True,
-    max_documents: Optional[int] = None,
-    pretrained_model_name_or_path: Optional[str] = None,
+    openwebtext_path: Path = typer.Argument(..., help="Path to a OpenWebText dump."),
+    output_filepath: Path = typer.Argument(..., help="Filepath to save the preprocessed text"),
+    min_length: Optional[int] = typer.Option(
+        None, help="Minimum token length of documents to retain"
+    ),
+    lowercase: bool = typer.Option(True, help="Whether text should be lowercased"),
+    max_documents: Optional[int] = typer.Option(
+        None,
+        help="Maximum number of documents to retain. Because of batching, this won't be exact.",
+    ),
+    pretrained_model_name_or_path: Optional[str] = typer.Option(
+        None,
+        help=(
+            "Name of the HuggingFace Tokenizer to use when determining the token length of a"
+            "document. Has no effect if min-length is None"
+        ),
+    ),
 ) -> None:
     """Lightly preprocesses an OpenWebText dump obtained from
     https://skylion007.github.io/OpenWebTextCorpus/. If `min-length is not None`, only documents
@@ -51,6 +42,8 @@ def main(
     no effect if `min-length is None`.
     """
     openwebtext_path = Path(openwebtext_path)
+    output_filepath = Path(output_filepath)
+    output_filepath.parents[0].mkdir(parents=True, exist_ok=True)
 
     # Setup the pre-trained tokenizer, if specified
     if min_length is not None:
@@ -59,14 +52,13 @@ def main(
             # user doesn't want to use it.
             from transformers import AutoTokenizer
 
-            tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path).tokenize
+            tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path, use_fast=True)
         else:
             tokenizer = lambda x: x.split()  # noqa
     else:
         tokenizer = None
 
-    early_exit = False
-    documents = []
+    processed_docs = 0
     skipped_files = 0
     typer.secho(
         (
@@ -79,52 +71,63 @@ def main(
     with typer.progressbar(
         length=max_documents or len(list(openwebtext_path.iterdir())), label="Preprocessing text"
     ) as progress:
-        for i, tar_filepath in enumerate(openwebtext_path.iterdir()):
+        for tar_filepath in openwebtext_path.iterdir():
             # Didn't bother debugging as it only happens for a tiny number (1-2) of tar archives.
             # Instead, catch the error and report to the user at the end how many we skipped.
             untared_filepath = Path(tar_filepath.stem)
             try:
-                with tarfile.open(tar_filepath) as f:
-                    f.extractall(untared_filepath)
+                with tarfile.open(tar_filepath) as tf:
+                    tf.extractall(untared_filepath)
             except (tarfile.ReadError, IsADirectoryError):
                 skipped_files += 1
                 continue
 
-            for text_filepath in untared_filepath.iterdir():
-                text = text_filepath.read_text()
-                text = sanitize(text, lowercase=lowercase)
-                if not text:
-                    continue
+            for text_filepaths in chunked(untared_filepath.iterdir(), 128):
+                docs = []
+                for fp in text_filepaths:
+                    # Some very minimal preprocessing to remove extra whitespace, newlines and tabs.
+                    doc = sanitize_text(fp.read_text(), lowercase=lowercase)
+                    # We add a space in front of the text in order to achieve consistant tokenization
+                    # with certain tokenizers, e.g. the BPE tokenizer used by RoBERTa, GPT and others.
+                    # See: https://github.com/huggingface/transformers/issues/1196
+                    doc = f"{ doc.lstrip()}"
+                    docs.append(doc)
 
-                # Retain documents if the length of their shortest document is
-                # equal to or greater than the minimum specified length
                 if tokenizer is not None:
-                    num_tokens = len(tokenizer(text))
-                    if min_length and num_tokens < min_length:
-                        continue
+                    if pretrained_model_name_or_path:
+                        lengths = tokenizer(
+                            docs, add_special_tokens=False, truncation=False, return_length=True
+                        ).length
+                    else:
+                        lengths = [len(tokenizer(doc)) for doc in docs]
+                    docs = [doc for doc, length in zip(docs, lengths) if length > min_length]
 
-                documents.append(text)
+                with open(output_filepath, "a") as f:
+                    f.write("\n".join(docs).strip() + "\n")
+
                 if max_documents:
+                    progress.update(len(docs))
+                    processed_docs += len(docs)
+                    if processed_docs >= max_documents:
+                        break
+
+            # We are using a for-else trick here, see: https://stackoverflow.com/a/3150107/6578628
+            else:
+                if max_documents is None:
                     progress.update(1)
-
-                if max_documents and len(documents) == max_documents:
-                    early_exit = True
-                    break
-
+                shutil.rmtree(untared_filepath)
+                # Continue if the inner loop wasn't broken.
+                continue
             shutil.rmtree(untared_filepath)
-            if max_documents is None:
-                progress.update(1)
-            if early_exit:
-                break
+            # Inner loop was broken, break the outer.
+            break
 
-    if skipped_files > 0:
+    if skipped_files:
         typer.secho(
             f"{WARNING} {skipped_files} tar files were skipped because they couldn't be extracted.",
             fg=typer.colors.YELLOW,
             bold=True,
         )
-
-    _write_output_to_disk(documents, output_filepath)
 
 
 if __name__ == "__main__":
